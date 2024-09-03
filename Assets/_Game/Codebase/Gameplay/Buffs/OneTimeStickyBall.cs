5af902f6dac3468722424b7449aa5ada2548c820
gkonoplya <info@sreda.games>
2024-09-03 14:08:26 +0300

    Starting ball velocity now depends on paddle movement

--- Assets/_Game/Codebase/Gameplay/Buffs/OneTimeStickyBall.cs
+++ Assets/_Game/Codebase/Gameplay/Buffs/OneTimeStickyBall.cs
@@ -5,15 +5,17 @@ namespace Gameplay.Buffs
     public class OneTimeStickyBall: StickyBall
     {
         public float startingForce = 5f;
+        private Rigidbody2D _paddleRb;
 
         protected override void Execute(GameObject target, GameObject self)
         {
+            _paddleRb = target.GetComponent<Rigidbody2D>();
             base.Execute(target, self);
-            savedVelocity = Vector2.up * startingForce;
         }
 
         protected override void ActivateBall(Rigidbody2D rb)
         {
+            savedVelocity = new Vector2(_paddleRb.linearVelocity.x, startingForce);
             base.ActivateBall(rb);
             Destroy(this);
         }
