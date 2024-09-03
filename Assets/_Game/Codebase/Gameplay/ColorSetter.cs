fdbd32596a7b999a21df78147ad2fbffb70f4397
gkonoplya <info@sreda.games>
2024-09-03 15:13:19 +0300

    Added some color to blocks

---
+++ Assets/_Game/Codebase/Gameplay/ColorSetter.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Infrastructure.Utils;
+using UnityEngine;
+
+namespace Gameplay
+{
+    [RequireComponent(typeof(SpriteRenderer))]
+    public class ColorSetter : MonoBehaviour
+    {
+        public List<Color> colorList;
+
+        private void Start() => 
+            GetComponent<SpriteRenderer>().color = colorList.Random();
+    }
+}
---
+++ Assets/_Game/Codebase/Infrastructure/Utils/ListExtensions.cs
@@ -0,0 +1,10 @@
+﻿using System.Collections.Generic;
+
+namespace Infrastructure.Utils
+{
+    public static class ListExtensions
+    {
+        public static T Random<T>(this IList<T> list) => 
+            list[UnityEngine.Random.Range(0, list.Count)];
+    }
+}
