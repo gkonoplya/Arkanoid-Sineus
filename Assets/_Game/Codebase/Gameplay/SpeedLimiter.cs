f9603bff2ddfc5f0dc766b81b9b32aea53c5a050
gkonoplya <info@sreda.games>
2024-09-03 15:38:49 +0300

    Added speed limiter for preventing ball stuck on too low speed

---
+++ Assets/_Game/Codebase/Gameplay/SpeedLimiter.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Gameplay
+{
+    [RequireComponent(typeof(Rigidbody2D))]
+    public class SpeedLimiter: MonoBehaviour
+    {
+        [Range(1f, 100f)]
+        public float minSpeed = 1f;
+        [Range(1f, 100f)]
+        public float maxSpeed = 100f;
+
+        public bool debugInfo = false;
+        public float currentSpeed;
+        private Rigidbody2D _rb;
+
+        private void Start()
+        {
+            _rb = GetComponent<Rigidbody2D>();
+
+            _rb.ObserveEveryValueChanged(rb => rb.linearVelocity.magnitude)
+                .Where(magnitude => magnitude < minSpeed || magnitude > maxSpeed)
+                .Subscribe(_ => LimitSpeed())
+                .AddTo(this);
+        }
+
+        private void Update()
+        {
+            if (!debugInfo) return;
+            currentSpeed = _rb.linearVelocity.magnitude;
+        }
+
+        private void LimitSpeed()
+        {
+            float speed = _rb.linearVelocity.magnitude;
+            float limitingFactor = 1f;
+            
+            if (speed < minSpeed) 
+                limitingFactor = Mathf.Approximately(speed, 0f) ? minSpeed : minSpeed / speed;
+            else if (speed > maxSpeed) 
+                limitingFactor = maxSpeed / speed;
+
+            _rb.linearVelocity = limitingFactor * _rb.linearVelocity;
+        }
+    }
+}
