9afd443658e28f1838ca78a0634710bdf1a6d6c7
gkonoplya <info@sreda.games>
2024-09-03 14:08:45 +0300

    blocks now damageable

---
+++ Assets/_Game/Codebase/Gameplay/Buffs/DamageDealer.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Buffs
+{
+    public class DamageDealer: CollisionBuff
+    {
+        public float amount;
+
+        protected override void Execute(GameObject target, GameObject self)
+        {
+            if (!target.TryGetComponent<Health>(out var health))
+                return;
+
+            health.ApplyDamage(amount);
+        }
+    }
+}
---
+++ Assets/_Game/Codebase/Gameplay/Buffs/Health.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using UniRx;
+using UnityEngine;
+
+namespace Gameplay.Buffs
+{
+    public class Health: MonoBehaviour
+    {
+        public float amount;
+        private IDisposable _observable;
+
+        private void OnEnable()
+        {
+            _observable = this
+                .ObserveEveryValueChanged(health => health.amount)
+                .Where(health => health <= 0)
+                .Subscribe(_ => gameObject.SetActive(false));
+        }
+
+        private void OnDisable()
+        {
+            _observable.Dispose();
+        }
+
+        public void ApplyDamage(float damage)
+        {
+            amount -= damage;
+        }
+    }
+}
