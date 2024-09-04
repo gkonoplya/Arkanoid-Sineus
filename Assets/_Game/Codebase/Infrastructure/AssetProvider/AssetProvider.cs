9695d7f8fa294ed9a15a436551f68d8a97ccdab5
gkonoplya <info@sreda.games>
2024-09-04 15:54:34 +0300

    Asset provider added

---
+++ Assets/_Game/Codebase/Infrastructure/AssetProvider/AssetProvider.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+namespace Infrastructure.AssetProvider
+{
+    [CreateAssetMenu(fileName = "Assets", menuName = "StaticData/Settings/Asset provider", order = 50)]
+    public class AssetProvider : ScriptableObject
+    {
+        [SerializeField] private AssetsDB _assets;
+        
+
+        public GameObject GetPrefab(string id) => 
+            _assets[id];
+
+        public T GetComponentOnPrefab<T>(string id) where T : Component => 
+            _assets[id].GetComponent<T>();
+        
+        
+    }
+}
---
+++ Assets/_Game/Codebase/Infrastructure/AssetProvider/AssetsDB.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using RotaryHeart.Lib.SerializableDictionary;
+using UnityEngine;
+
+namespace Infrastructure.AssetProvider
+{
+    [Serializable]
+    public class AssetsDB : SerializableDictionaryBase<string, GameObject>
+    {
+    }
+}
