8eac12c3be46e529d1f4db245c33db31f9a05c68
gkonoplya <info@sreda.games>
2024-09-04 17:18:41 +0300

    Monobehviour helpers added

---
+++ Assets/_Game/Codebase/Infrastructure/Utils/MonoBehaviourHelpers.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+namespace Infrastructure.Helpers
+{
+  public static class MonoBehaviourHelpers
+  {
+    public static void DestroyGO(this Object obj) => Object.Destroy(obj);
+    public static void DestroyGO(this Object obj, float delay) => Object.Destroy(obj, delay);
+    
+    public static Vector3 WorldPosition(this MonoBehaviour obj) => obj.transform.position;
+    public static Vector3 LocalPosition(this MonoBehaviour obj) => obj.transform.localPosition;
+
+    public static bool ActiveInHierarchy(this MonoBehaviour obj) =>
+      !obj.IsNullOrDestroyed() && obj.isActiveAndEnabled &&
+      obj.gameObject.activeInHierarchy;
+
+    public static bool IsNullOrDestroyed(this object obj) =>
+      obj == null || obj.Equals(null);
+  }
+}
