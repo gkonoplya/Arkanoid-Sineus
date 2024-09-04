bd298fd77c48d0d5b885fbcda458af875fab48c8
gkonoplya <info@sreda.games>
2024-09-04 19:58:07 +0300

    Sprite switcher & powerblock sprite switcher

---
+++ Assets/_Game/Codebase/Gameplay/Level/PowerBlockSpriteSwitcher.cs
@@ -0,0 +1,31 @@
+﻿using Gameplay.Buffs;
+using Infrastructure.Utils;
+using UniRx;
+using UnityEngine;
+
+namespace Gameplay.Level
+{
+    public class PowerBlockSpriteSwitcher: BlockSpriteSwitcher
+    {
+        private float _maxHealth;
+        private SpriteRenderer _renderer;
+
+        private void Start()
+        {
+            var health = GetComponent<Health>();
+            _maxHealth = health.MaxHealth;
+            
+            health.amount
+                .TakeWhile(x => x > Constants.Epsilon)
+                .Subscribe(ChangeSprite)
+                .AddTo(this);
+            _renderer = GetComponent<SpriteRenderer>();
+        }
+
+        private void ChangeSprite(float currentHealth)
+        {
+            int index = Mathf.CeilToInt(currentHealth / _maxHealth);
+            _renderer.sprite = Sprites[index];
+        }
+    }
+}
