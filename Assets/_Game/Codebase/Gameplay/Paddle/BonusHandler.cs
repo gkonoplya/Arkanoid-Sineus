b5dacaab7b07bc94cead99e26204e2c5f5265652
gkonoplya <info@sreda.games>
2024-09-04 15:58:42 +0300

    Bonus handler

---
+++ Assets/_Game/Codebase/Gameplay/Paddle/BonusHandler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Gameplay.Buffs;
+using UnityEngine;
+
+namespace Gameplay
+{
+    public class BonusHandler: MonoBehaviour
+    {
+
+        private void OnTriggerEnter2D(Collider2D other)
+        {
+            if (!other.TryGetComponent<Bonus>(out var bonus)) 
+                return;
+            
+            bonus.ApplyBonus();
+            Destroy(bonus.gameObject,0.1f);
+        }
+    }
+}
