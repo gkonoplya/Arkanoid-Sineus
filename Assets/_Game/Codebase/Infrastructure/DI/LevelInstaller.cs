22540559a6afb0436073859be1d01b6ea610c76f
gkonoplya <info@sreda.games>
2024-09-04 15:59:08 +0300

    wiring up all with scene settings & levelinstallers registering

--- Assets/_Game/Codebase/Infrastructure/DI/LevelInstaller.cs
+++ Assets/_Game/Codebase/Infrastructure/DI/LevelInstaller.cs
@@ -1,10 +1,21 @@
+using Gameplay.Buffs;
+using Gameplay.Data;
+using Infrastructure.AssetProvider;
 using Infrastructure.InputService;
 using Zenject;
 
 public class LevelInstaller : MonoInstaller
 {
+    public LevelData levelData;
+    public AssetProvider assetProvider;
+
     public override void InstallBindings()
     {
         Container.BindInterfacesAndSelfTo<StandaloneInputService>().AsSingle();
+        Container.Bind<BonusFactory>().AsSingle();
+        Container.Bind<BuffFactory>().AsSingle();
+        Container.BindInstance(levelData).AsSingle();
+        Container.BindInstance(assetProvider).AsSingle();
+
     }
 }
