6740774a19a9b03a2bf4c34587c0c7f9f3b63c3a
gkonoplya <info@sreda.games>
2024-09-04 15:55:42 +0300

    Level data & levelDataProvider added

---
+++ Assets/_Game/Codebase/Gameplay/Data/LevelData.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using UnityEngine;
+
+namespace Gameplay.Data
+{
+    [Serializable]
+    public class LevelData
+    {
+        public GameObject Paddle;
+        public GameObject Ball;
+    }
+}
---
+++ Assets/_Game/Codebase/Gameplay/Data/LevelDataProvider.cs
@@ -0,0 +1,9 @@
+﻿using UnityEngine;
+
+namespace Gameplay.Data
+{
+    public class LevelDataProvider: MonoBehaviour
+    {
+        public LevelData levelData;
+    }
+}
